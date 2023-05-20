f0c97bef8897ee35ff8a8f9f87822f2d7a8c1c3d
Juan José Pallarés Sánchez <juanjo.palla1@gmail.com>
2023-05-20 12:37:40 +0200

    Push changes for frontal camera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent {
       video: {
         width: { min: 1024, ideal: 1920, max: 1920 },
         height: { min: 576, ideal: 1080, max: 1080 },
-        //facingMode: 'environment'
+        facingMode: 'environment'
       },
     };
 
